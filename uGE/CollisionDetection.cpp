30231dca83640b9bd5af8f5990878cadd69fb2f6
Dyanketo <Dyanketo@hotmail.com>
2015-02-11 22:42:50 +0100

    SphereCollider v0.7 working check.

--- uGE/CollisionDetection.cpp
+++ uGE/CollisionDetection.cpp
@@ -1,5 +1,8 @@
 #include "CollisionDetection.hpp"
 
+#include "utils/glm.hpp"
+
+#include "SphereCollider.hpp"
 #include "GameObject.hpp"
 #include <vector>
 
@@ -18,11 +21,45 @@ namespace uGE{
       void CollisionDetection::update(std::vector<GameObject *> _objects){
 
             //std::cout<< _objects.size() <<std::endl;
-        ////for( GameObject* object : _objects ) {
-            //object->getPosition(); //used to get each object position.
+            std::vector<Collider *> colliderArray;
+        for( GameObject* object : _objects ) {
+             //used to get each object position.
+           std::vector<Collider *> tempArray;
+           tempArray = object->getCollider();//should be a perfect copy of the collider array of the object
 
-            ////// ->std::cout << object->getPosition() << std::endl;
+            colliderArray.insert(colliderArray.end(),tempArray.begin(),tempArray.end());
 
-        ////}//end of for-loop-all GO's
+            for (unsigned int i = 0; i<colliderArray.size(); i++){
+                for (unsigned int j = i+1; j<colliderArray.size() ; j++ ){
+
+                    SphereCollider * colliderA = dynamic_cast <SphereCollider *>(colliderArray[i]);
+                    //SphereCollider* collA = dynamic_cast<SphereCollider*>( colliderArray[i] );
+                    SphereCollider* colliderB = dynamic_cast <SphereCollider *>(colliderArray[j]);
+
+                    checkSphereCollision(colliderA ,colliderB);
+                }//end of J for loop
+            }//end of I for loop
+        }//end of for-loop-all GO's
       }//end of update();
+
+      void CollisionDetection::checkSphereCollision(SphereCollider * sphere1 ,SphereCollider * sphere2){
+
+        float sumOfRadius = sphere1->getRadius() + sphere2->getRadius();
+
+        //sumOfRadius* = sumOfRadius;
+
+        glm::vec3 difference = sphere2->getPosition() -  sphere1->getPosition();
+
+        float dist = difference.length();
+
+        if(dist <= sumOfRadius){
+
+            std::cout<< "printing collision"<< std::endl;
+        }
+
+        //distance = (float radius2 - float radius1);
+
+      //do math here
+      }
+
 }//end of namespace
