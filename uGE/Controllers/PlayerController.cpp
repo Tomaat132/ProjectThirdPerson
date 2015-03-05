f439d1cc3aafb2be85f2a1675d9b43c1c2e1e1f4
Karanum <hyougakazu@gmail.com>
2015-03-05 10:41:21 +0100

    Tiny fixes

--- uGE/Controllers/PlayerController.cpp
+++ uGE/Controllers/PlayerController.cpp
@@ -61,7 +61,7 @@ namespace uGE {
        //transform[2][0] = sin(rotate);
         //transform[2][2] = -sin(rotate+90);
 		// note, does not check collision, just moves on xz plane !
-		if( keyW || keyS || keyA || keyD) transform = glm::translate( transform, glm::vec3(0, 0, 1.f) );
+		if( keyW || keyS || keyA || keyD) transform = glm::translate( transform, glm::vec3(0, 0, 1.f) * speed );
 		if(rotate != glm::vec3(0,0,0)) _parent->setRotation(rotate);
 		//transform = glm::rotate( transform, rotate, glm::vec3( 0,1,0 ) );
 	}
@@ -71,7 +71,7 @@ namespace uGE {
         uGE::GameObject * particleEmitter = new uGE::GameObject( "ParticleEmitter");
 
             particleEmitter->setController( new uGE::ParticleEmitterController( particleEmitter, _parent) );
-           uGE::SceneManager::add( particleEmitter );
+            uGE::SceneManager::add( particleEmitter );
 	}
 	void PlayerController::shoot()
 	{
@@ -89,6 +89,6 @@ namespace uGE {
 
 	void PlayerController::onCollision( CollisionResult * result )
 	{
-        std::cout << result->objectB->getName() << std::endl;
+        //std::cout << result->objectB->getName() << std::endl;
 	}
 }
